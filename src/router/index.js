cb0aedb554dac5bc19587ad61266239d0f391dfe
DLineone <DLineone@users.noreply.github.com>
2024-05-16 16:20:26 +0300

    init

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import MainPage from '@/views/MainPage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: MainPage
+    }
+  ]
+})
+
+export default router
